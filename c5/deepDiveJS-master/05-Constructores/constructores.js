ee290c53b164aaac5934b43dc8952e10ce646b11
Mao <mauricio.vera.r7@gmail.com>
2022-06-19 14:47:38 -0400

    m3_c6_constructores

--- c5/deepDiveJS-master/05-Constructores/constructores.js
+++ c5/deepDiveJS-master/05-Constructores/constructores.js
@@ -1,11 +1,14 @@
-class CalculadoraNPI { // clase
+// ul expect(calculadora instanceof CalculadoraNPI).toEqual(true);
+// expect(calculadora.valor()).toEqual(5);
+class CalculadoraNPI { // clase Notacion Polaca Inversa
+  
   constructor () {
     this.num1 = undefined
     this.num2 = undefined
     this.valor_interno = 0
   }
 
-  agregar() { // metodo
+  agregar(num) { // metodo
     if (this.num1 == undefined) {
       this.num1 = num
     } else {
