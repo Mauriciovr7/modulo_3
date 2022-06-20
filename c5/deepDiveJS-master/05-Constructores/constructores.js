cf6c7376c39ce81b5a72c9f3d83a362ad3dc3b82
Mao <mauricio.vera.r7@gmail.com>
2022-06-20 10:10:44 -0400

    m3_c6_constru

--- c5/deepDiveJS-master/05-Constructores/constructores.js
+++ c5/deepDiveJS-master/05-Constructores/constructores.js
@@ -5,19 +5,28 @@ class CalculadoraNPI { // clase Notacion Polaca Inversa
   constructor () {
     this.num1 = undefined
     this.num2 = undefined
+    this.num3 = undefined
     this.valor_interno = 0
   }
 
   agregar(num) { // metodo
     if (this.num1 == undefined) {
       this.num1 = num
-    } else {
+    } else if (this.num2 == undefined) {
       this.num2 = num
+    } else {
+      this.num3 = num
     }
   }
 
-  sumar() {
-    this.valor_interno = this.num1 + this.num2
+  sumar() {    
+    if (this.num3 == undefined) {
+      this.valor_interno = this.num1 + this.num2
+    } else if (this.valor_interno == this.num2 + this.num3) {
+      this.valor_interno = this.num1 + this.num2 + this.num3
+    } else {
+      this.valor_interno = this.num2 + this.num3
+    }
   }
 
   valor() { // retornar valor interno
